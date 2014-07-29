9f88d84a653dfab6dfcdb87f3ace0af43993dd11
zaccary price <zaccary.price@avansera.com>
2014-07-29 15:14:37 +0300

    add feed and feed-item markup and styles

--- app/scripts/controllers/main.controller.js
+++ app/scripts/controllers/main.controller.js
@@ -1,12 +1,6 @@
 'use strict';
 
-/**
- * @ngdoc function
- * @name potatoFlickrApp.controller:MainCtrl
- * @description
- * # MainCtrl
- * Controller of the potatoFlickrApp
- */
+
 angular.module('Potato.Controllers')
 	.controller('MainCtrl', ['$scope', 'FlickrService', function($scope, FlickrService) {
 		$scope.errors	= [];
